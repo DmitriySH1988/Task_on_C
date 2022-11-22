6e8cb8a7884cc46234d9b3f2f47863ff0efcfee9
Dmitriy <Dimon8803@yandex.ru>
2022-11-22 18:55:19 +0300

    editing Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -9,9 +9,9 @@ int num_3 = Convert.ToInt32(Console.ReadLine()); // Entering a number
 
 if (num_1 > num_2 || num_1 > num_3)
     Console.Write($"Максимальное число = {num_1}");
-else if (num_2 > num_2 || num_2 > num_3)
+else if (num_2 > num_1 || num_2 > num_3)
     Console.Write($"Максимальное число = {num_2}");
-else if (num_3 > num_2 || num_3 > num_3)
+else if (num_3 > num_1 || num_3 > num_2)
     Console.Write($"Максимальное число = {num_3}");
 else
     Console.WriteLine("Числа равны");
