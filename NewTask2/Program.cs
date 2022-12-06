0ea559e4fa1a4da9f5ed50a4d2598fd116c158e2
Dmitriy <Dimon8803@yandex.ru>
2022-12-06 18:47:09 +0300

    New Tasks home work

---
+++ NewTask2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 100);
+}
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        count+= array[i];
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите колличество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {ReleaseArray(array)}");
---
+++ NewTask3/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+void InputArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+}
+double ReleaseArray(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        else if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    Console.WriteLine($"всего {array.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+    return max - min;
+}
+
+Console.Clear();
+Console.Write("Введите колличество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {ReleaseArray(array)}");
