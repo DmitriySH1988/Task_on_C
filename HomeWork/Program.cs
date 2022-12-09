b03dec5c6cb0472fef6ec09659554699211614d8
Dmitriy <Dimon8803@yandex.ru>
2022-12-09 23:12:48 +0300

    New Home Work,2

---
+++ HomeWork/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+    Console.Clear();
+    Console.Write("Введите кол-во цифр в массиве: ");
+    int elementsCount = int.Parse(Console.ReadLine());
+    int[] array = new int[elementsCount];
+    int count = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"\nВведите элемент массива под индексом {i}: ");
+        array[i] = int.Parse(Console.ReadLine());
+        if (array[i] >=0)
+        count++;
+    }
+
+    Console.WriteLine($"Вывод массива: [{string.Join(", ", array)}] - чисел больше 0 = {count}");
